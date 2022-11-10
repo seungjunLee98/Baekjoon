7eb1e3fa5ff794b233b9f0c378dbb94a74a088a5
RubenBoard <klsj9810@gmail.com>
2022-11-11 02:07:50 +0900

    boj2738

---
+++ 22.11.07~11.13/boj2738.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = []
+b = []
+
+for _ in range(n):
+    a.append(list(map(int, input().split())))
+for _ in range(n):
+    b.append(list(map(int, input().split())))
+
+for i in range(n):
+    for j in range(m):
+        c = a[i][j] + b[i][j]
+        if m!=(j+1):
+            print(c, end=' ')
+        else:
+            print(c)
+
+
+
