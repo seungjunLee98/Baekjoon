678281c7b3ace9b4b409857559cac6091515f0c5
RubenBoard <klsj9810@gmail.com>
2023-01-04 17:39:17 +0900

    boj25501

---
+++ 23.01.02~01.08/boj25501.py
@@ -0,0 +1,17 @@
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    global cnt
+    cnt = 0
+    return recursion(s, 0, len(s)-1)
+
+n = int(input())
+
+for _ in range(n):
+    s = input()
+    print(isPalindrome(s), cnt)
