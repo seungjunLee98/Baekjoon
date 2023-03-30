857d44d0cb5bda59c172bd1e1ce7de2d8037173a
RubenBoard <klsj9810@gmail.com>
2023-03-30 10:07:44 +0900

    boj4796

---
+++ 23.03.16~03.31/boj4796.py
@@ -0,0 +1,14 @@
+l, p, v = 1, 1, 1
+cnt = 0
+while True:
+    l, p, v = map(int, input().split())
+    if l==0 and p==0 and v==0:
+        break
+    cnt+=1
+    ans = (v//p)*l
+    if v%p < l:
+        ans += v%p
+    else:
+        ans += l
+    print(f"Case {cnt}: {ans}")
+
