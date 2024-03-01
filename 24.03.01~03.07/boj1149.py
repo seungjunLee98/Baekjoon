af03c513e97c6fab917662bdb1ffd9c5d985300e
이승준 <klsj9810@gmail.com>
2024-03-01 22:57:33 +0900

    dd

--- 24.03.01~03.07/boj1149.py
+++ 24.03.01~03.07/boj1149.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = []
+for _ in range(n):
+    d.append(list(map(int, input().split())))
+for i in range(1, n):
+    d[i][0] += min(d[i-1][1], d[i-1][2])
+    d[i][1] += min(d[i-1][2], d[i-1][0])
+    d[i][2] += min(d[i-1][0], d[i-1][1])
+
+result = min(d[n-1])
+print(result)
